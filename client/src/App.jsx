c8be61c60406d4c7fcabaff4f008c2c9dfb2b072
Huzaima Muhammad <huzaimamuhammad73@gmail.com>
2023-03-16 20:21:53 +0100

    url changed

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -17,7 +17,7 @@ function App() {
 
     const fetchBotResponse = async () => {
         const { data } = await axios.post(
-            "https://chatgpt-app-jsqv.onrender.com",
+            "https://chatgpt-app-f529.onrender.com",
             // "http://localhost:5173",
             { input },
             {
